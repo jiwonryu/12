0152a8664e7c29b26bb8b2038ce1592182b27f3d
jiwonryu <rjwbp1@naver.com>
2018-11-22 15:16:24 +0900

    1 문자열 복사

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main (void){
+	char src[100] = "Programming course";
+	char dst[100];
+	int i=0;
+	
+	while ( src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i]='\0';
+	
+	printf("dst : %n", dst);
+	
+	return 0;
+}
+
