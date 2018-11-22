27099804db78919c35fed5e8f690127405c1b772
jiwonryu <rjwbp1@naver.com>
2018-11-22 15:36:34 +0900

    입력받은 단어 파일로 출력

--- main.c
+++ main.c
@@ -5,18 +5,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main (void){
-	char src[100] = "Programming course";
-	//char dst[100];
-	//int i=0;
+	FILE *fp;
+	char input[100];
+	int i;
 	
-	/*while ( src[i] != '\0')
+	fp= fopen("sample.tst","w");
+	
+	for (i=0;i<3;i++)
 	{
-		dst[i] = src[i];
-		i++;
+		printf("input a word : ");
+		scanf("%s",input);
+		fprintf(fp, "%s\n", input);
 	}
-		dst[i]='\0';*/
 	
-	printf("문자열\"%s\"의 길이 : %i" , src, strlen(src));
+	fclose(fp);
 	
 	return 0;
 }
